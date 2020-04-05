ccb4a9f81de2a487f3ffc08fc59cbe70979f98e3
Philipp Thiel <Taronyu@users.noreply.github.com>
2020-04-05 19:52:41 +0200

    Repository work

--- src/main/java/org/dapnet/core/model/Repositories.java
+++ src/main/java/org/dapnet/core/model/Repositories.java
@@ -1,22 +1,57 @@
 package org.dapnet.core.model;
 
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public interface Repositories {
+public final class Repositories {
 
-	ReadWriteLock getLock();
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
+	private final UserRepository userRepo;
+	private final CallSignRepository callSignRepo;
+	private final TransmitterRepository transmitterRepo;
+	private final NodeRepository nodeRepo;
+	private final TransmitterGroupRepository groupRepo;
+	private final RubricRepository rubricRepo;
 
-	UserRepository getUsers();
+	/**
+	 * Creates a new repositories object.
+	 */
+	public Repositories() {
+		userRepo = new UserRepositoryImpl();
+		callSignRepo = new CallSignRepositoryImpl();
+		transmitterRepo = new TransmitterRepositoryImpl();
+		nodeRepo = new NodeRepositoryImpl();
+		groupRepo = new TransmitterGroupRepositoryImpl();
+		rubricRepo = new RubricRepositoryImpl();
+	}
 
-	CallSignRepository getCallsigns();
+	public ReadWriteLock getLock() {
+		return lock;
+	}
 
-	TransmitterRepository getTransmitters();
+	public UserRepository getUsers() {
+		return userRepo;
+	}
 
-	NodeRepository getNodes();
+	public CallSignRepository getCallsigns() {
+		return callSignRepo;
+	}
 
-	TransmitterGroupRepository getTransmitterGroups();
+	public TransmitterRepository getTransmitters() {
+		return transmitterRepo;
+	}
 
-	RubricRepository getRubrics();
+	public NodeRepository getNodes() {
+		return nodeRepo;
+	}
+
+	public TransmitterGroupRepository getTransmitterGroups() {
+		return groupRepo;
+	}
+
+	public RubricRepository getRubrics() {
+		return rubricRepo;
+	}
 
 //	EntityRepository<Call> getCalls();
 //
